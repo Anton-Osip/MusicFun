706fb44e8660cfadce7063266973599ccfebb4e2
Anton-Osip <kepamuk@yandex.ru>
2025-11-03 11:19:06 +0300

    8 / Уcловный рендеринг / Алгоритм и структуры данных / Курс React Путь Самурая: Без альтернатив

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,7 @@ const tracks = [
         title: "Musicfun soundtrack instrumental",
         url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
     },
-    {
-        id: 4,
-        title: "Musicfun soundtrack instrumental",
-        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
+
 ]
 
 function App() {
