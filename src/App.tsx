07255d562a3a4c7a5aa4c7cf9098d9cc3f346607
Anton-Osip <kepamuk@yandex.ru>
2025-11-03 11:01:09 +0300

    8 / Уcловный рендеринг / Алгоритм и структуры данных / Курс React Путь Самурая: Без альтернатив

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,48 @@
 import './App.css'
 
+const tracks = [
+    {
+        id: 1,
+        title: "Musicfun soundtrack",
+        url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+        id: 2,
+        title: "Musicfun soundtrack instrumental",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+    {
+        id: 3,
+        title: "Musicfun soundtrack instrumental",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+    {
+        id: 4,
+        title: "Musicfun soundtrack instrumental",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+]
+
 function App() {
-    const tracks = [
-        {
-            id: 1,
-            title: "Musicfun soundtrack",
-            url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-        },
-        {
-            id: 2,
-            title: "Musicfun soundtrack instrumental",
-            url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-        },
-        {
-            id: 3,
-            title: "Musicfun soundtrack instrumental",
-            url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-        },
-        {
-            id: 4,
-            title: "Musicfun soundtrack instrumental",
-            url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-        },
-    ]
+    const selectedTrackId = 1
 
     return (
         <>
-            <h1>Musicfun player</h1>
-            <ul>
-                {tracks.map((track) => (
-                    <li key = {track.id}>
-                        <div>{track.title}</div>
-                        <audio controls src = {track.url}></audio>
-                    </li>
-                ))}
-            </ul>
+            <h1>MusicFun player</h1>
+            {tracks.length === null
+                ? <span>Loading...</span>
+                : tracks.length === 0
+                    ? <span>No tracks</span>
+                    : <ul>
+                        {tracks.map((track) => (
+                            <li key = {track.id}
+                                style = {{border: selectedTrackId === track.id ? '1px solid orange' : 'none'}}>
+                                <div>{track.title}</div>
+                                <audio controls src = {track.url}></audio>
+                            </li>
+                        ))}
+                    </ul>}
+
         </>
     )
 }
