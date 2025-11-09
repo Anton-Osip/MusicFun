e78828c2c5d0c9745246150a5ea3d54eaf5a4dca
Anton-Osip <osipchika@gmail.com>
2025-11-09 16:30:22 +0300

    14 / Falsy, Truthy, Условный рендеринг часть 2 / Курс React Путь Самурая: Без альтернатив

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 
 
 function App() {
-    const [selectedTrackId, setSelectedTrackId] = useState<number | null>(null)
+    const [selectedTrack, setSelectedTrack] = useState(null)
+    const [selectedTrackId, setSelectedTrackId] = useState(null)
     const [tracks, setTracks] = useState([])
 
     useEffect(() => {
@@ -18,14 +19,6 @@ function App() {
             .then(data => setTracks(data.data))
     }, [])
 
-    const changeSelectedTrackId = (trackId: number | null) => {
-        if (trackId === selectedTrackId) {
-            setSelectedTrackId(null)
-        } else {
-            setSelectedTrackId(trackId)
-        }
-
-    }
 
     return (
         <>
@@ -34,17 +27,50 @@ function App() {
                 ? <span>Loading...</span>
                 : tracks.length === 0
                     ? <span>No tracks</span>
-                    : <ul>
-                        {tracks.map((track) => (
-                            <li key = {track.id}
-                                style = {{border: selectedTrackId === track.id ? '1px solid orange' : 'none'}}>
-                                <div onClick = {() => {
-                                    changeSelectedTrackId(track.id)
-                                }}>{track.attributes.title}</div>
-                                <audio controls src = {track.attributes.attachments[0].url}></audio>
-                            </li>
-                        ))}
-                    </ul>}
+
+                    : <div style = {{display: 'flex', gap: '10px', justifyContent: 'space-between'}}>
+
+                        <div>
+                            <button onClick = {() => {
+                                setSelectedTrack(null)
+                                setSelectedTrackId(null)
+                            }}>reselect
+                            </button>
+                            <ul>
+                                {tracks.map((track) => (
+                                    <li key = {track.id}
+                                        style = {{border: selectedTrackId === track.id ? '1px solid orange' : 'none'}}>
+                                        <div onClick = {() => {
+                                            setSelectedTrackId(track.id)
+                                            fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${track.id}`,
+                                                {
+                                                    headers: {
+                                                        'api-key': '6658d6dc-bb62-4b5e-9850-168b231c09b5'
+                                                    }
+                                                }
+                                            )
+                                                .then(res => res.json())
+                                                .then(data => setSelectedTrack(data.data))
+                                            setSelectedTrack(track)
+                                        }}>{track.attributes.title}</div>
+                                        <audio controls src = {track.attributes.attachments[0].url}></audio>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Details</h2>
+                            {!selectedTrack && !selectedTrackId && 'track is not selected'}
+                            {!selectedTrack && selectedTrackId && 'Loading...'}
+                            {selectedTrack && selectedTrackId && selectedTrack.id !== selectedTrackId && 'Loading...'}
+                            {selectedTrack && <div>
+                                <h3>{selectedTrack.attributes.title}</h3>
+                                <h4>lyrics</h4>
+                                <p>{selectedTrack.attributes.lyrics ?? 'no lyrics'}</p>
+                            </div>}
+
+                        </div>
+                    </div>}
 
         </>
     )
