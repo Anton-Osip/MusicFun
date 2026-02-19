f8391e1984aff3a20f204fc3f301d6ac1c78c42a
Anton-Osip <osipchika@gmail.com>
2026-02-19 12:53:41 +0300

    create actions

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -1,46 +1,50 @@
 export type GetTrackDetailsOutputData = {
-    id: string
-    attributes: {
-        title: string
-        lyrics: string | null
-    }
+	id: string
+	attributes: {
+		title: string
+		lyrics: string | null
+	}
 }
 
 type AttachmentDto = {
-    url: string
+	url: string
 }
 
 type TrackListItemOutputAttributes = {
-    title: string
-    attachments: Array<AttachmentDto>
+	title: string
+	attachments: Array<AttachmentDto>
 }
 
 export type TrackListItemOutput = {
-    id: string
-    attributes: TrackListItemOutputAttributes
+	id: string
+	attributes: TrackListItemOutputAttributes
 }
 
 export type GetTrackDetailsOutput = { data: GetTrackDetailsOutputData }
 export type GetTrackListItemOutput = { data: TrackListItemOutput[] }
 
 export const getTracks = async (): Promise<GetTrackListItemOutput> => {
-    const res = await fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
-        {
-            headers: {
-                'api-key': '6658d6dc-bb62-4b5e-9850-168b231c09b5'
-            }
-        }
-    )
-    return await res.json()
+	const res = await fetch(
+		'https://musicfun.it-incubator.app/api/1.0/playlists/tracks',
+		{
+			headers: {
+				'api-key': '6658d6dc-bb62-4b5e-9850-168b231c09b5',
+			},
+		},
+	)
+	return await res.json()
 }
 
-export const getTrack = async (trackId: string): Promise<GetTrackDetailsOutput> => {
-    const res = await fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`,
-        {
-            headers: {
-                'api-key': '6658d6dc-bb62-4b5e-9850-168b231c09b5'
-            }
-        }
-    )
-    return await res.json()
+export const getTrack = async (
+	trackId: string,
+): Promise<GetTrackDetailsOutput> => {
+	const res = await fetch(
+		`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/${trackId}`,
+		{
+			headers: {
+				'api-key': '6658d6dc-bb62-4b5e-9850-168b231c09b5',
+			},
+		},
+	)
+	return await res.json()
 }
